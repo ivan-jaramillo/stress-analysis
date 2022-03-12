dadd0faddd97f56342002da9a3f50f5d1c0177b4
Ivan Jaramillo <ivandavilajaramillo@gmail.com>
2022-03-12 16:46:04 -0600

    Import useful packages
    TensorFlow and NumPy will be handy in this project.

--- stress_analysis/stress_analysis.py
+++ stress_analysis/stress_analysis.py
@@ -1,5 +1,14 @@
+"""Stress Analysis
+
+Calculate stress at a point on a body.
+"""
+
+import tensorflow as tf
+import numpy as np
+
 def main():
     print("Stress Analysis at a Point")
+    
 
 
 if __name__ == "__main__":
