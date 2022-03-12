dabd2d694bde16a5bae41e34f7d6f5b7297e16a6
Ivan Jaramillo <ivandavilajaramillo@gmail.com>
2022-03-12 16:57:44 -0600

    Have user input principal stresses
    Nothing too crazy here.

--- stress_analysis/stress_analysis.py
+++ stress_analysis/stress_analysis.py
@@ -8,7 +8,15 @@ import numpy as np
 
 def main():
     print("Stress Analysis at a Point")
-    
+
+    # Have the user input principal stressses for now.
+    sigma_xx = int(input("sigma_xx = "))
+    sigma_yy = int(input("sigma_yy = "))
+    sigma_zz = int(input("sigma_zz = "))
+
+    # Create a rank two tensor for stress.
+    stress = np.array([(sigma_xx, 0, 0), (0, sigma_yy, 0), (0, 0, sigma_zz)])
+    print(stress)
 
 
 if __name__ == "__main__":
