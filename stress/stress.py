6e51e69ac272aa5bb26cb5612123de17211672e0
Quinten <quinten@giglio.us>
2022-05-18 13:30:39 -0700

    numpy array inheritance and principle calculations

--- stress/stress.py
+++ stress/stress.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-class Stress:
-
-    def __init__(self):
-        pass
+class stress(np.ndarray):
+    # Inherits Numpy array class
+    #
+        def __new__(cls, a):
+            # Create Array Object
+            obj = np.asarray(a).view(cls)
+            # Calculate a few things
+            w,v = np.linalg.eig(obj)
+            obj.principal_stress = w
+            obj.principal_axes = v 
+            return obj
+        
